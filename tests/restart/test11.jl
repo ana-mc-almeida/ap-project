a48b6a760b4c9b7bc963f047834b66ffd419df91
Ana Margarida Almeida <ana.margarida2004@gmail.com>
2025-03-17 12:50:51 +0000

    Update restart test10 and add restart test 11

---
+++ tests/restart/test11.jl
@@ -0,0 +1,14 @@
+include("../../Exceptional.jl")
+
+reciprocal(value) =
+    with_restart(:return_zero => () -> 0,
+        :return_value => identity,
+        :retry_using => reciprocal) do
+        value == 0 ?
+        error(DivisionByZero()) :
+        1 / value
+    end + 1
+
+handling(DivisionByZero => (c) -> invoke_restart(:return_zero)) do
+    println(reciprocal(0))
+end
