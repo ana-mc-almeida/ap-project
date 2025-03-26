26cb19b15c6c1c3455aae0e2af0698fc3b981c94
Ana Margarida Almeida <ana.margarida2004@gmail.com>
2025-03-26 18:52:11 +0000

    Update restart tests

--- tests/restart/test13.jl
+++ tests/restart/test13.jl
@@ -1,13 +1,13 @@
-include("../../Exceptional2.jl")
+include("./common.jl")
 
 struct TestException <: Exception end
 
 handling(TestException => (c) -> invoke_restart(:return_value, 10)) do
     handling(DivisionByZero => (c) -> invoke_restart(:return_value, 5)) do
-        with_restart(:return_zero => () -> 0,
-                    :return_value => identity,
-                    :retry_using => reciprocal) do
+        println(with_restart(:return_zero => () -> 0,
+            :return_value => identity,
+            :retry_using => reciprocal) do
             throw(TestException())
-        end
+        end)
     end
 end
