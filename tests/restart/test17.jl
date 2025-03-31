19e6df0361698e553ca98829c3e125d3f1832e25
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-03-31 17:33:31 +0100

    test17 refactor

--- tests/restart/test17.jl
+++ tests/restart/test17.jl
@@ -5,11 +5,9 @@ handling(DivisionByZero => (x) ->
         invoke_restart(:return_zero)
     end
 ) do
-    result = 0
     println(with_restart(:return_zero => () -> 0) do
         with_restart(:return_zero => () -> 0) do
-            result += 1
-        end
-        result += 1
+            1
+        end + 1
     end)
 end
